32f4c1adfd349cb90b11dbbb102e86ca1ea07059
brenobiancardi <brenobiancardi@outlook.com>
2021-02-04 00:29:20 -0300

    funçoes de conversao criadas

--- README.md
+++ README.md
@@ -6,14 +6,16 @@ Fui desafiado por [Camila Ribeiro](https://www.linkedin.com/in/camila-ferreira-r
 
 ## Desafio
 
-1.  Crie uma aplicação Angular (Não esqueça que todo código precisa estar no GitHub)
+- [x] Crie uma aplicação Angular (Não esqueça que todo código precisa estar no GitHub)
 
 2.  Crie no mínimo 3 componentes que deverão estar interagindo na mesma página (Não esqueça das rotas)
 
 3.  Escolha no mínimo 4 componentes do Angular Material e incorpore eles em sua aplicação.
 
+    > 1/4 MatSnackBar
+
 4.  Crie em uma das páginas uma interação através da Interpolação e/ou utilizando Property Biding.
 
-5.  Crie uma explicação sobre o que foi feito, esta explicação será inserida na plataforma junto com o link para o código no GitHub.
+- [x] Crie uma explicação sobre o que foi feito, esta explicação será inserida na plataforma junto com o link para o código no GitHub.
 
-6.  Você poderá criar um vídeo demonstrativo da aplicação construída, e publicá-lo no YouTube, passando o link de acesso na plataforma. Este passo 6, não é obrigatório, mas entrará como um diferencial.
+- [x] Você poderá criar um vídeo demonstrativo da aplicação construída, e publicá-lo no YouTube. (Optei por disponibilizar a aplicação em um servidor para testar)
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,55 +1,110 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EMPTY } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HomeService {
-  constructor() {}
+  constructor(private snackBar: MatSnackBar) {}
+
+  showMessage(msg: string, isError: boolean = false): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 10000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success'],
+    });
+  }
+
+  errorHandler(msg: string, isError: boolean = true): typeof EMPTY {
+    this.showMessage(msg, isError);
+
+    return EMPTY;
+  }
 
   arrayToString(object: Object): string {
     const line = Object.values(object);
     return '"' + line.join('","') + '"';
   }
 
-  toCSV(jsonString: string): string {
-    try {
-      const a = JSON.parse(jsonString);
-      let saida = '';
-
-      if (a.length) {
-        let i = 0;
-        saida = this.arrayToString(Object.keys(a[i])) + '\n';
-        while (i < a.length) {
-          saida += this.arrayToString(a[i]) + '\n';
-          i++;
-        }
-      } else {
-        saida = this.arrayToString(Object.keys(a)) + '\n';
-        saida += this.arrayToString(a);
+  convertToCSV(json): string {
+    let output: string = '';
+    if (json.length) {
+      let i = 0;
+      output = this.arrayToString(Object.keys(json[i])) + '\n';
+      while (i < json.length) {
+        output += this.arrayToString(json[i]) + '\n';
+        i++;
       }
-      return saida;
+    } else {
+      output = this.arrayToString(Object.keys(json)) + '\n';
+      output += this.arrayToString(json);
+    }
+    return output;
+  }
+
+  toCSV(jsonString: string): string {
+    let JsonParsed = '';
+    try {
+      JsonParsed = JSON.parse(jsonString);
     } catch {
-      return 'JSON não é valido';
+      JsonParsed = String(`[${jsonString}]`);
+    }
+    try {
+      JsonParsed = JSON.parse(JsonParsed);
+    } catch {
+      this.errorHandler('JSON não é valido');
+      return 'Erro ao converter estrutura de dados';
+    } finally {
+      let saida = this.convertToCSV(JsonParsed);
+      this.errorHandler('Conversão realizada', false);
+      return saida;
     }
   }
 
   toJson(CSVString: string): string {
-    let lines = CSVString.split('\n');
-    let jsonObject = {};
-    let props = lines.shift().split(',');
-    const keysProps = props;
-    while (props.length > 0) {
-      const prop = props.shift();
-      Object.defineProperty(jsonObject, prop, {
-        enumerable: true,
-        configurable: true,
-        writable: true,
-      });
+    try {
+      JSON.parse(CSVString);
+      this.errorHandler('Já é um JSON valido', false);
+      return CSVString;
+    } catch {
+      let arrayJsonObjects = [];
+
+      let lines = CSVString.split('\n');
+      let props = lines.shift().split(',');
+      let i = 0;
+      if (lines.length === 0) {
+        this.errorHandler(
+          'CSV deve ser composto de um header e pelo menos uma linha de dados',
+          true
+        );
+        return 'CSV Invalido';
+      }
+      while (lines.length > 0) {
+        const line = lines.shift().split(',');
+        let jsonObject = {};
+        i++;
+        try {
+          props.forEach((value) => {
+            let prop = value.split('"').join('');
+            jsonObject[prop] = line.shift().split('"').join('');
+          });
+
+          arrayJsonObjects.push(jsonObject);
+        } catch {
+          console.log(`Linha ${i} removida.`);
+        }
+      }
+      const JSONsaida = JSON.stringify(arrayJsonObjects, null, 2);
+
+      try {
+        JSON.parse(JSONsaida);
+        return JSONsaida;
+      } catch {
+        this.errorHandler('Não conseguimos converter seu CSV :(');
+        return 'Erro ao converter estrutura de dados';
+      }
     }
-    while (lines.length > 0) {
-      const line = lines.shift().split(',');
-      // line.map((value,index) =>
-    }
-    return 'string';
   }
 }
